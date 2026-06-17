b5d9fda965385858c4ee3219d824b634d6ae6e58
bo-nicolaisen <bni@techcollege.dk>
2026-06-17 11:08:37 +0200

    full burger

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,9 @@
 // write cool JS hwere!!
+const burgerMenu = document.getElementById('burger-menu');
+
+const overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
